6efc6861730056fa0cda85b9076203dd3d601309
rpiontik <r.piontik@mail.ru>
2026-05-10 19:23:05 +0300

    Updated gradle config for 2026.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.grammarkit.tasks.GenerateLexerTask
 import org.jetbrains.grammarkit.tasks.GenerateParserTask
 
@@ -6,7 +7,7 @@ plugins {
     id("java")
     id("idea")
     id("org.jetbrains.kotlin.jvm")
-    id("org.jetbrains.intellij")
+    id("org.jetbrains.intellij.platform")
     id("org.jetbrains.grammarkit")
 }
 
@@ -17,114 +18,99 @@ val platformType: String by project
 val platformPlugins: String by project
 val flexPath: String by project
 val genLexerPath: String by project
-val genLexerClassName: String by project
-val genLexerPurgeOldFiles: String by project
 val bnfPath: String by project
 val genParserClassPath: String by project
 val genPsiPath: String by project
-val genParserPurgeOldFiles: String by project
 val kotlinVersion: String by project
 val JSONataVersion: String by project
 val pluginSinceBuild: String by project
 val pluginUntilBuild: String by project
 val plantumlVersion: String by project
 val elkVersion: String by project
+val jflexVersion: String by project
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
 }
 
 dependencies {
-
-    /**
-     * Базовые зависимости
-     */
-    implementation ("com.ibm.jsonata4java:JSONata4Java:$JSONataVersion")
+    intellijPlatform {
+        intellijIdea("2026.1.1")
+        val pluginsList: List<String> = platformPlugins.split(',').map { it.trim() }.filter { it.isNotEmpty() };
+        bundledPlugins( pluginsList )
+        jetbrainsRuntime()
+    }
+    implementation("org.jetbrains.intellij.deps.jflex:jflex:$jflexVersion")
+    implementation("com.ibm.jsonata4java:JSONata4Java:$JSONataVersion")
     implementation("net.sourceforge.plantuml:plantuml:$plantumlVersion")
     implementation("org.eclipse.elk:org.eclipse.elk.alg.layered:$elkVersion")
     implementation("org.eclipse.elk:org.eclipse.elk.core:$elkVersion")
     implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-    implementation("org.javassist:javassist:3.29.2-GA")
+    // implementation("org.javassist:javassist:3.29.2-GA")
 
-    /**
-     * Тестовые зависимости
-     */
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+
 }
 
-intellij {
-    version.set(platformVersion)
-    type.set(platformType)
-    plugins.set(platformPlugins.split(',').map(String::trim).filter(String::isNotEmpty))
-}
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set(pluginSinceBuild)
+            untilBuild.set(pluginUntilBuild)
+        }
+        changeNotes.set(provider {
+            file("src/main/resources/html/change-notes.html").readText()
+        })
+    }
 
+}
 
 sourceSets {
     main {
-        java {
-            srcDir(genPath)
-        }
+        java { srcDir(genPath) }
     }
 }
 
-tasks.jar {
-    manifest.attributes["PLANTUML_LIMIT_SIZE"] = "24384"
-}
-
-val generateJSONataLexer =  tasks.register<GenerateLexerTask>("genJSONataLexer") {
-
-    description = "Generated Jsonata lexer"
-    group = "build setup"
-    sourceFile.set(File(flexPath))
-    targetOutputDir.set(File(genLexerPath))
-    purgeOldFiles.set(genLexerPurgeOldFiles.toBoolean())
-}
-
-val generateJSONataParser = tasks.register<GenerateParserTask>("genJSONataParser") {
-    dependsOn(generateJSONataLexer)
-    description = "Generated Jsonata parser"
-    group = "build setup"
-
-    pathToParser.set(genParserClassPath)
-    pathToPsiRoot.set(genPsiPath)
-    purgeOldFiles.set(genParserPurgeOldFiles.toBoolean())
-
-    sourceFile.set(File(bnfPath))
-    targetRootOutputDir.set(File(genPath))
-    pathToParser.set(genParserClassPath)
-    pathToPsiRoot.set(genPsiPath)
-    sourceFile.set(project.file(bnfPath))
-    targetRootOutputDir.set(project.file(genPath))
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 tasks {
-
-    buildSearchableOptions {
-        enabled = false
-    }
-
     withType<JavaCompile> {
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
+        sourceCompatibility = "21"
+        targetCompatibility = "21"
         options.encoding = "UTF-8"
-        dependsOn(generateJSONataParser)
+        dependsOn("genJSONataParser")
     }
 
-    withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-        dependsOn(generateJSONataParser)
+    withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+        dependsOn("genJSONataParser")
     }
 
-    patchPluginXml {
-        sinceBuild.set(pluginSinceBuild)
-        untilBuild.set(pluginUntilBuild)
-        changeNotes.set(file("src/main/resources/html/change-notes.html").readText())
+    // Твои таски генерации
+    register<GenerateLexerTask>("genJSONataLexer") {
+        sourceFile.set(project.file(flexPath))
+        targetOutputDir.set(project.file(genLexerPath))
+        purgeOldFiles.set(true)
     }
 
-    test {
-        useJUnitPlatform()
+    register<GenerateParserTask>("genJSONataParser") {
+        dependsOn("genJSONataLexer")
+        sourceFile.set(project.file(bnfPath))
+        targetRootOutputDir.set(project.file(genPath))
+        pathToParser.set(genParserClassPath)
+        pathToPsiRoot.set(genPsiPath)
+        purgeOldFiles.set(true)
     }
 }
-
-
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,32 @@
-pluginManagement {
+rootProject.name = "IDEAPlugin"
 
+pluginManagement {
     repositories {
-        repositories {
-            maven {
-                url = uri("https://plugins.gradle.org/m2/")
-            }
-        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
     }
 
-    val kotlinVersion: String by settings
-    val gradleIntellijPluginVersion: String by settings
-    val grammarKitVersion: String by settings
-
     plugins {
-        id("org.jetbrains.kotlin.jvm") version kotlinVersion
-        id("org.jetbrains.intellij") version gradleIntellijPluginVersion
-        id("org.jetbrains.grammarkit") version grammarKitVersion
+        id("org.jetbrains.kotlin.jvm") version providers.gradleProperty("kotlinVersion").get()
+        id("org.jetbrains.intellij.platform.settings") version providers.gradleProperty("gradleIntellijPluginVersion").get()
+        id("org.jetbrains.intellij.platform") version providers.gradleProperty("gradleIntellijPluginVersion").get()
+        id("org.jetbrains.grammarkit") version providers.gradleProperty("grammarKitVersion").get()
+        id("org.jetbrains.intellij.deps.jflex") version providers.gradleProperty("jflexVersion").get()
     }
 }
 
-rootProject.name = "IDEAPlugin"
+plugins {
+    id("org.jetbrains.intellij.platform.settings") version providers.gradleProperty("gradleIntellijPluginVersion").get()
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+// Этот блок подтянет всё остальное (включая JFlex для тестов/сборки)
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://www.jetbrains.com/intellij-repository/releases")
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+    }
+}
